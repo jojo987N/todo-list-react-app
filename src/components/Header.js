1213541473b11b4cb97afe17ea4dc046560db33a
jemba narcisse <cabliveer@gmail.com>
2023-06-06 12:51:31 +0200

    Add header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <>
+      <li className="item1">
+        Today&apos;s To Do
+        <i className="material-icons">sync</i>
+      </li>
+      <li>
+        <hr />
+      </li>
+    </>
+  );
+}
